8106f8b31e16d128e25a9ed5d1ddd30a60f02c15
erosrolf <erosrolf@gmail.com>
2024-10-11 13:52:14 +0300

    добавил проект Avalonia для тестирования движка

---
+++ GameEngine.Template/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GameEngine.Template.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ GameEngine.Template/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GameEngine.Template.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
